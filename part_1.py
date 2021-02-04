d84a2e6bfac55ad3529826b353e5517f5c14f742
Francisco Ferrari <33513662+franciscojferrari@users.noreply.github.com>
2021-02-04 16:50:54 +0100

    3.1

--- part_1.py
+++ part_1.py
@@ -5,9 +5,11 @@ from sklearn.metrics import mean_squared_error
 import sys
 
 
-def sin(x_range_start: float, x_range_end: float, x_step: float):
+def sin(x_range_start: float, x_range_end: float, x_step: float, noise = False):
     x = np.arange(x_range_start, x_range_end + x_step, x_step)
-    return {"x": x, "y": np.sin(2 * x)}
+    noise_values = np.random.normal(0, 0.1, len(x))
+    y = np.sin(2 * x) + noise_values if noise else 0
+    return {"x": x, "y": y}
 
 
 def square(x_range_start: float, x_range_end: float, x_step: float):
@@ -42,6 +44,30 @@ def predict(w, rbf_nodes, y):
     return {"y": predictions, "total_error": total_error}
 
 
+def batch_learning(train_data, val_data):
+    residual_errors = {"train": [], "val": []}
+    for n in range(1, 20):
+        rbf_nodes_train = generate_rbf_nodes(train_data, n)
+        rbf_nodes_val = generate_rbf_nodes(val_data, n)
+        w = train(train_data['y'], rbf_nodes_train)
+        predictions_train = predict(w, rbf_nodes_train, train_data['y'])
+        predictions_val = predict(w, rbf_nodes_val, val_data['y'])
+        residual_errors["train"].append(predictions_train["total_error"])
+        residual_errors["val"].append(predictions_train["total_error"])
+        print(f'N: {n} - Residual Error: {predictions_train["total_error"]}')
+        plt.plot(train_data["x"], train_data["y"], label = "Train")
+        plt.plot(train_data["x"], predictions_train["y"], label = "Train Predictions")
+        plt.plot(val_data["x"], predictions_val["y"], label = "Val Predictions")
+        plt.title(f"Function : N. of RBF Nodes: {n}")
+        plt.legend()
+        plt.show()
+
+    # plt.plot(residual_errors["train"])
+    # print(residual_errors["val"])
+    # plt.plot(residual_errors["val"])
+    # plt.show()
+
+
 def main():
     x_train_start, x_val_start, x_train_end, x_val_end = 0, 0.05, math.pi, math.pi
 
@@ -49,26 +75,7 @@ def main():
     val_sin = sin(x_val_start, x_val_end, 0.1)
     train_square = square(x_train_start, x_train_end, 0.1)
     val_square = square(x_val_start, x_val_end, 0.1)
-
-    residual_errors = {"train": [], "val": []}
-    for n in range(1, 20):
-        rbf_nodes_train = generate_rbf_nodes(train_sin, n)
-        rbf_nodes_val = generate_rbf_nodes(val_sin, n)
-        w = train(train_sin['y'], rbf_nodes_train)
-        predictions_train = predict(w, rbf_nodes_train, train_sin['y'])
-        predictions_val = predict(w, rbf_nodes_val, val_sin['y'])
-        residual_errors["train"].append(predictions_train["total_error"])
-        residual_errors["val"].append(predictions_train["total_error"])
-
-        # plt.plot(train_sin["x"], train_sin["y"], label = "Train")
-        # plt.plot(train_sin["x"], predictions_train["y"], label = "Train Predictions")
-        # plt.plot(val_sin["x"], predictions_val["y"], label = "Val Predictions")
-        # plt.title(f"Sin Wave : N. of RBF Nodes: {n}")
-        # plt.legend()
-
-    # plt.plot(residual_errors["train"])
-    plt.plot(residual_errors["val"])
-    plt.show()
+    batch_learning(train_square, val_square)
 
 
 if __name__ == '__main__':
