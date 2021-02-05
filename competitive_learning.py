7ec45f9a2046761c85880cfac97cdd9f74723b72
AleixEF <aleix.fontcuberta@gmail.com>
2021-02-05 16:29:42 +0100

    almost done with all class methods

--- competitive_learning.py
+++ competitive_learning.py
@@ -19,6 +19,7 @@ Parameters:
 """
 
 import numpy as np
+import random
 
 class CompetitiveLearning(object):
     def __init__(self, num_clusters, init_type="samples", num_winners = 1):
@@ -42,11 +43,47 @@ class CompetitiveLearning(object):
         return
     
     def update_winners(self, sample, l_rate):
+        indexes_winners = find_closest_centers(sample, self.centers, 
+                                                              self.num_winners)
+        for idx in indexes_winners:
+            self.centers[idx, :] += l_rate * (sample - self.centers[idx, :])  
         return
     
     def uniform_init(self, samples):
         return
     
-    def choose_random_samples(samples, self):
-        return
+    def choose_random_samples(self, samples):
+        num_samples = samples.shape[0]
+        sample_indexes = [i for i in range(num_samples)]
+        # random.sample returns k non repated indexes chosen randomly 
+        random_indexes = random.sample(sample_indexes, k=self.num_clusters)
+        random_samples = samples[random_indexes]
+        return random_samples
+        
+# closest in the sense of euclidean distance
+# if num_closest = 1, the closest center is returned.
+# if num_closest = 2, the 2 closest centers are returned, etc
+def find_closest_centers(sample, centers, num_closest):
+    indexes_closest = np.zeros(num_closest, dtype=np.int32)
+    # 1d array, sample square distance respect to each center
+    square_distances = np.sum((centers-sample)**2, axis=1)
+    for i in range(num_closest):
+        idx_closest = np.argmin(square_distances)
+        indexes_closest[i] = idx_closest
+        # to avoid finding the same minimum in the next iter
+        square_distances[idx_closest] = np.inf
+    return indexes_closest
+
+"""
+centers = np.array([[1., 2], [-1, 0], [-1, 2]])
+sample = np.array([1, 2.])
+closest_centers = find_closest_centers(sample, centers, num_closest=2)
+print(closest_centers)
+"""
+cl = CompetitiveLearning(num_clusters=2)
+
+samples = np.array([[1., 2], [-1, 0], [-1, 2]])
+random_samples = cl.choose_random_samples(samples)
+print(random_samples)
+
 
