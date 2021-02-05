baac2e23e28a9666060c7f0db6986acaf1ba7de3
Francisco Ferrari <33513662+franciscojferrari@users.noreply.github.com>
2021-02-05 10:01:50 +0100

    new version 3.1 and 3.2

--- part_1.py
+++ part_1.py
@@ -2,60 +2,61 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
-import sys
+from utils import sin, square
 
 
-def sin(x_range_start: float, x_range_end: float, x_step: float, noise = False):
-    x = np.arange(x_range_start, x_range_end + x_step, x_step)
-    noise_values = np.random.normal(0, 0.1, len(x))
-    y = np.sin(2 * x) + (noise_values if noise else 0)
-    return {"x": x, "y": y}
+class rbfNN:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        # self.mus = np.linspace(min(self.x), max(self.x), self.n)
+        if "sigma" not in kwargs:
+            self.sigma = 0.1
+        if "epochs" not in kwargs:
+            self.epochs = 50
+        if "lr" not in kwargs:
+            self.lr = 0.2
 
+    @staticmethod
+    def calculate_rfb(x, mu_i, sigma):
+        return np.exp((-(x - mu_i) ** 2) / (2 * sigma ** 2))
 
-def square(x_range_start: float, x_range_end: float, x_step: float, noise = False):
-    x = np.arange(x_range_start, x_range_end, x_step)
-    noise_values = np.random.normal(0, 0.1, len(x))
-    sin = np.sin(2 * x)
-    sin[sin >= 0] = 1
-    sin[sin < 0] = -1
-    sin = sin + (noise_values if noise else 0)
-    return {"x": x, "y": sin}
+    def generate_rbf_nodes(self, values_x, n):
+        rfb = lambda x, mui: rbfNN.calculate_rfb(x, mui, self.sigma)
+        rbf_nodes = np.array([rfb(x, mu) for x in values_x for mu in self.mus])
+        rbf_nodes = rbf_nodes.reshape(len(values_x), n)
+        return rbf_nodes
 
+    def train_batch(self, n):
+        self.n = n
+        self.mus = np.linspace(min(self.x), max(self.x), self.n)
+        phi = self.generate_rbf_nodes(self.x, self.n)
+        self.w = np.linalg.inv(phi.T @ phi) @ (phi.T @ self.y)
 
-def calculate_rfb(x, mu_i, sigma):
-    return np.exp((-(x - mu_i) ** 2) / (2 * sigma ** 2))
+    def predict(self, x, y):
+        predictions = self.generate_rbf_nodes(x, self.n) @ self.w
+        total_error = mean_squared_error(y, predictions)
+        return {"y": predictions, "total_error": total_error}
 
+    def train_delta(self, n):
+        self.n = n
+        self.mus = np.linspace(min(self.x), max(self.x), self.n)
+        w = np.random.randn(n)
 
-def generate_rbf_nodes(values_x, n):
-    mus = np.linspace(min(values_x), max(values_x), n)
-    sigma = 0.1
-    rfb = lambda t, mui: calculate_rfb(t, mui, sigma)
-    rbf_nodes = np.array([rfb(x, mu) for x in values_x for mu in mus])
-    rbf_nodes = rbf_nodes.reshape(len(values_x), n)
-    return rbf_nodes
-
-
-def train(y, rbf_nodes):
-    w = np.linalg.inv(rbf_nodes.T @ rbf_nodes) @ (rbf_nodes.T @ y)
-    return w
-
-
-def predict(w, rbf_nodes, y):
-    predictions = rbf_nodes @ w
-    total_error = mean_squared_error(y, predictions)
-    return {"y": predictions, "total_error": total_error}
+        for i in range(self.epochs):
+            for k, (x_k, y_k) in enumerate(zip(self.x, self.y)):
+                phi_k = self.generate_rbf_nodes([x_k], self.n)
+                w += self.lr * (y_k - phi_k @ w) @ phi_k
+        self.w = w
 
 
 def batch_learning(train_data, val_data):
     residual_errors = {"train": [], "val": []}
+    rbfnn = rbfNN(x = train_data['x'], y = train_data['y'])
+
     for n in range(1, 20):
-        rbf_nodes_train = generate_rbf_nodes(train_data['x'], n)
-        rbf_nodes_val = generate_rbf_nodes(val_data['x'], n)
-
-        w = np.linalg.inv(rbf_nodes_train.T @ rbf_nodes_train) @ (rbf_nodes_train.T @ train_data['y'])
-
-        predictions_train = predict(w, rbf_nodes_train, train_data['y'])
-        predictions_val = predict(w, rbf_nodes_val, val_data['y'])
+        rbfnn.train_batch(n)
+        predictions_train = rbfnn.predict(train_data['x'], train_data['y'])
+        predictions_val = rbfnn.predict(val_data['x'], val_data['y'])
         residual_errors["train"].append(predictions_train["total_error"])
         residual_errors["val"].append(predictions_train["total_error"])
         print(f'N: {n} - Residual Error: {predictions_train["total_error"]}')
@@ -73,23 +74,21 @@ def batch_learning(train_data, val_data):
 
 
 def delta_learning(train_data, val_data, lr = 0.2):
-    n = 20
-    rbf_nodes_train = generate_rbf_nodes(train_data['x'], n)
-    rbf_nodes_val = generate_rbf_nodes(val_data['x'], n)
+    rbfnn = rbfNN(x = train_data['x'], y = train_data['y'], lr = 0.1, epochs = 40, sigma = 0.08)
+    residual_errors = {"train": [], "val": []}
+    for n in range(19, 20):
+        rbfnn.train_delta(n = n)
+        predictions_train = rbfnn.predict(train_data['x'], train_data['y'])
+        predictions_val = rbfnn.predict(val_data['x'], val_data['y'])
+        residual_errors["train"].append(predictions_train["total_error"])
+        residual_errors["val"].append(predictions_train["total_error"])
 
-    w = np.random.randn(n)
-    for i in range(100):
-        for k, (x_k, y_k) in enumerate(zip(train_data['x'], train_data['y'])):
-            w += lr * (y_k - rbf_nodes_train[k] @ w) * rbf_nodes_train[k]
-
-    predictions_train = predict(w, rbf_nodes_train, train_data['y'])
-    predictions_val = predict(w, rbf_nodes_val, val_data['y'])
-
-    plt.plot(train_data["x"], train_data["y"], label = "Train")
-    plt.plot(train_data["x"], predictions_train["y"], label = "Train Predictions")
-    plt.plot(val_data["x"], predictions_val["y"], label = "Val Predictions")
-    plt.legend()
-    plt.show()
+        plt.plot(train_data["x"], train_data["y"], label = "Train")
+        plt.plot(train_data["x"], predictions_train["y"], label = "Train Predictions")
+        plt.plot(val_data["x"], predictions_val["y"], label = "Val Predictions")
+        plt.title(f"Delta : N. of RBF Nodes: {n}")
+        plt.legend()
+        plt.show()
 
 
 def main():
@@ -100,7 +99,7 @@ def main():
     val_sin = sin(x_val_start, x_val_end, 0.1, noise = noise)
     train_square = square(x_train_start, x_train_end, 0.1, noise = noise)
     val_square = square(x_val_start, x_val_end, 0.1, noise = noise)
-    # batch_learning(train_square, val_square)
+    # batch_learning(train_sin, val_sin)
     delta_learning(train_sin, val_sin)
     # plt.plot(train_square["x"], train_square["y"], label = "Train")
     # plt.show()
--- utils.py
+++ utils.py
@@ -1,2 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def sin(x_range_start: float, x_range_end: float, x_step: float, noise = False):
+    x = np.arange(x_range_start, x_range_end + x_step, x_step)
+    noise_values = np.random.normal(0, 0.1, len(x))
+    y = np.sin(2 * x) + (noise_values if noise else 0)
+    return {"x": x, "y": y}
+
+
+def square(x_range_start: float, x_range_end: float, x_step: float, noise = False):
+    x = np.arange(x_range_start, x_range_end, x_step)
+    noise_values = np.random.normal(0, 0.1, len(x))
+    sin = np.sin(2 * x)
+    sin[sin >= 0] = 1
+    sin[sin < 0] = -1
+    sin = sin + (noise_values if noise else 0)
+    return {"x": x, "y": sin}
