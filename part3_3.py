7d8e34df01eaf2ebf35cb90ab853b7cf7a593c7a
AleixEF <aleix.fontcuberta@gmail.com>
2021-02-09 18:25:53 +0100

    cleaned code and found better widths

--- part3_3.py
+++ part3_3.py
@@ -8,39 +8,55 @@ Created on Tue Feb  9 15:52:44 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
-from competitive_learning import CompetitiveLearning, find_rbf_widths
+from competitive_learning import CompetitiveLearning
 from general_rbf import RBFNetwork
 from sklearn.metrics import mean_squared_error
 
 # We start with the sin2 part without noise
 
-start = 0
-shift = 0.05
-end = 2*np.pi
-step = 0.1
+def get_training_test():
+    start = 0
+    shift = 0.05
+    end = 2*np.pi
+    step = 0.1
+    
+    num_train = int(np.round((end-start)/step + 1))
+    x_train = np.linspace(start, end, num=num_train)
+    x_train = np.reshape(x_train, newshape=(num_train, 1))
+    y_train = np.sin(2*x_train)
+    
+    x_test = np.linspace(start+shift, end+shift, num_train)
+    x_test = np.reshape(x_test, newshape=(num_train, 1))
+    y_test = np.sin(2*x_test)
+    return (x_train, y_train), (x_test, y_test)
 
-num_train = int(np.round((end-start)/step + 1))
-x_train = np.linspace(start, end, num=num_train)
-x_train = np.reshape(x_train, newshape=(num_train, 1))
-y_train = np.sin(2*x_train)
+def get_rbf_centers(x_train, num_rbfs):
+    cl_epochs = 600
+    cl_rate = 0.01
+    cl = CompetitiveLearning(num_rbfs, init_type="samples")
+    centers = cl.fit(x_train, cl_epochs, cl_rate)
+    return centers
 
-x_test = np.linspace(start+shift, end+shift, num_train)
-x_test = np.reshape(x_test, newshape=(num_train, 1))
-y_test = np.sin(2*x_test)
+def plot_centers(centers):
+    plt.figure()
+    plt.title(str(centers.shape[0]) + " centers")
+    for i in range(centers.shape[0]):
+        plt.plot(centers[i], 0, "ro", markersize=3)
+    return
 
+num_rbfs = 11
+(x_train, y_train), (x_test, y_test) = get_training_test()
+centers = get_rbf_centers(x_train, num_rbfs)
+# so that our rbfs span the entire space
 
-num_rbfs = 18
-cl_epochs = 200
-cl_rate = 0.01
-cl = CompetitiveLearning(num_rbfs)
-centers = cl.fit(x_train, cl_epochs, cl_rate)
-widths = find_rbf_widths(centers)
+widths = (np.pi / num_rbfs) * np.ones(num_rbfs) 
+plot_centers(centers)
 
-rbf_epochs = 500
+rbf_epochs = 100
 rbf = RBFNetwork(centers, widths, output_dim=1)
 train_mse = rbf.fit((x_train, y_train), (x_test, y_test), rbf_epochs, verbose=False)[0]
-
 y_pred = rbf.predict(x_test)
+
 plt.figure()
 plt.xlabel("epoch")
 plt.title("Train MSE evolution")
@@ -55,23 +71,21 @@ plt.legend()
 test_mse = mean_squared_error(y_test, y_pred)
 print("test mse", test_mse)
 
+
 # Now we add noise to the training and test signals
+num_rbfs = 28
 variance = 0.1**2
 y_train += np.random.normal(scale=np.sqrt(variance), size=(y_train.shape))
 y_test += np.random.normal(scale=np.sqrt(variance), size=(y_test.shape))
- 
-"""
-plt.figure()
-plt.title("Noisy sin(2x)")
-plt.plot(x_train, y_train)
-plt.plot(x_test, y_test)
+centers = get_rbf_centers(x_train, num_rbfs) 
+plot_centers(centers)
+widths = (np.pi / num_rbfs) * np.ones(num_rbfs) 
 
-"""
-
+rbf_epochs = 200
 rbf = RBFNetwork(centers, widths, output_dim=1)
 train_mse = rbf.fit((x_train, y_train), (x_test, y_test), rbf_epochs, verbose=False)[0]
-
 y_pred = rbf.predict(x_test)
+
 plt.figure()
 plt.xlabel("epoch")
 plt.title("Train MSE evol with noise")
